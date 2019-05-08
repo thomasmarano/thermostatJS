6962fbf733aab822bb994494d39d238494838953
thomasmarano <thomas.marano2@gmail.com>
2019-05-08 14:16:07 +0100

    :poop: Add passing code to increase temperature by 1

--- jasmine-standalone-3.4.0/src/thermostat.js
+++ jasmine-standalone-3.4.0/src/thermostat.js
@@ -5,3 +5,7 @@ function Thermostat() {
 Thermostat.prototype.getCurrentTemperature = function(){
     return this.temperature;
 };
+
+Thermostat.prototype.increaseTemperature = function(){
+    return this.temperature +=  1;
+};
