aae5cd194abcfad943a031db922a5d67a3eaa13c
thomasmarano <thomas.marano2@gmail.com>
2019-05-08 18:15:38 +0100

    :recycle: Refactor variables

--- jasmine-standalone-3.4.0/src/thermostat.js
+++ jasmine-standalone-3.4.0/src/thermostat.js
@@ -2,6 +2,7 @@ function Thermostat() {
     this.DEFAULT_TEMP = 20
     this.temperature = this.DEFAULT_TEMP;
     this.MIN_TEMP = 10;
+    this.MEDIUM_TEMP_LIMIT = 18;
     this.MAX_TEMP = 32;
     this.PS_MAX_TEMP = 25;
     this.powerSaving = true;
@@ -65,14 +66,14 @@ Thermostat.prototype.getEnergyUsage = function(){
 };
 
 Thermostat.prototype.isLowUsage = function(){
-    return this.temperature < 18;
+    return this.temperature < this.MEDIUM_TEMP_LIMIT;
 }
 
 Thermostat.prototype.isMediumUsage = function(){
-    return this.temperature < 25;
+    return this.temperature < this.PS_MAX_TEMP;
 }
 
 Thermostat.prototype.isHighUsage = function(){
-    return this.temperature > 24;
+    return this.temperature > this.PS_MAX_TEMP;
 }
 
