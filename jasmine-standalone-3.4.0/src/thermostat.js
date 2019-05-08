2b71ba88b5cf222ca929f2062fbe3b86612d5a9b
thomasmarano <thomas.marano2@gmail.com>
2019-05-08 14:19:20 +0100

    :poop: Add passing code for adjusting temperature down by 1

--- jasmine-standalone-3.4.0/src/thermostat.js
+++ jasmine-standalone-3.4.0/src/thermostat.js
@@ -6,6 +6,10 @@ Thermostat.prototype.getCurrentTemperature = function(){
     return this.temperature;
 };
 
-Thermostat.prototype.increaseTemperature = function(){
-    return this.temperature +=  1;
+Thermostat.prototype.tempUp = function(){
+    this.temperature +=  1;
+};
+
+Thermostat.prototype.tempDown = function(){
+    this.temperature -= 1;
 };
